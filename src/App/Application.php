934dbf3ceb552b0b40ac84b704114c50480cfd14
kokocikys <chernecky_ilia@rambler.ru>
2021-04-05 18:01:06 +0300

    05.04.2021 18:00

---
+++ src/App/Application.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+/**
+ * Class Application
+ * @package App
+ */
+class Application
+{
+    /**
+     * @return boolean
+     */
+    public function test(): bool
+    {
+        return true;
+    }
+}

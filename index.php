023f3742223e750727596384013f7f94fbcc467c
Ilya Chernecky <chernecky_ilia@rambler.ru>
2021-05-13 16:47:29 +0300

    13.05.2021 16:50

--- index.php
+++ index.php
@@ -1,33 +1,8 @@
 <?php
-require $_SERVER["DOCUMENT_ROOT"] . '/vendor/autoload.php';
 
-$greetings = new App\Frase\Greetings();
-$showDate = new App\Action\Date();
-$logger = new App\Logger\Logger();
-$logger->logLoadedPage(__FILE__);
-?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PHP practice</title>
-</head>
-<body>
-<div>
-    <div>
-        <?= $greetings->sayHello() ?>
-    </div>
-    <div>
-        Current date: <?= $showDate->showDate() ?>
-    </div>
-</div>
+use Zend\Diactoros\ServerRequestFactory;
+use Zend\Diactoros\Response\HtmlResponse;
+use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
 
-<div>
-    <a href="/anotherPage/index.php">Go to another page!</a>
-</div>
-
-</body>
-</html>
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';

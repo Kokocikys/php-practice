830f274b084f4536af32401e2e6d541c1068c0bd
Ilya Chernecky <chernecky_ilia@rambler.ru>
2021-07-09 09:11:53 +0300

    test fix

--- tests/App/Http/Action/Blog/IndexActionTest.php
+++ tests/App/Http/Action/Blog/IndexActionTest.php
@@ -7,18 +7,22 @@ use PHPUnit\Framework\TestCase;
 
 class IndexActionTest extends TestCase
 {
-    public function testSuccess()
-    {
-        $action = new IndexAction();
-        $response = $action();
+	/**
+	 * @throws \JsonException
+	 */
+	public function testSuccess() : void
+	{
+		$action = new IndexAction();
+		$response = $action();
 
-        self::assertEquals(200, $response->getStatusCode());
-        self::assertJsonStringEqualsJsonString(
-            json_encode([
-                ['id' => 2, 'title' => 'The Second Post'],
-                ['id' => 1, 'title' => 'The First Post'],
-            ]),
-            $response->getBody()->getContents()
-        );
-    }
+		self::assertEquals( 200, $response->getStatusCode() );
+		self::assertJsonStringEqualsJsonString(
+			json_encode( [
+				             [ 'id' => 1, 'title' => 'The First Post' ],
+				             [ 'id' => 2, 'title' => 'The Second Post' ],
+			             ], JSON_THROW_ON_ERROR
+			),
+			$response->getBody()->getContents()
+		);
+	}
 }
--- tests/App/Http/Action/Blog/ShowActionTest.php
+++ tests/App/Http/Action/Blog/ShowActionTest.php
@@ -4,36 +4,39 @@ namespace Tests\App\Http\Action\Blog;
 
 use App\Http\Action\Blog\ShowAction;
 use PHPUnit\Framework\TestCase;
-use Zend\Diactoros\ServerRequest;
+use Laminas\Diactoros\ServerRequest;
 
 class ShowActionTest extends TestCase
 {
-    public function testSuccess()
-    {
-        $action = new ShowAction();
+	/**
+	 * @throws \JsonException
+	 */
+	public function testSuccess() : void
+	{
+		$action = new ShowAction();
 
-        $request = (new ServerRequest())
-            ->withAttribute('id', $id = 2);
+		$request = ( new ServerRequest() )
+			->withAttribute( 'id', $id = 2 );
 
-        $response = $action($request);
+		$response = $action( $request );
 
-        self::assertEquals(200, $response->getStatusCode());
-        self::assertJsonStringEqualsJsonString(
-            json_encode(['id' => $id, 'title' => 'Post #' . $id]),
-            $response->getBody()->getContents()
-        );
-    }
+		self::assertEquals( 200, $response->getStatusCode() );
+		self::assertJsonStringEqualsJsonString(
+			json_encode( [ 'id' => $id, 'title' => 'Post #' . $id ], JSON_THROW_ON_ERROR ),
+			$response->getBody()->getContents()
+		);
+	}
 
-    public function testNotFound()
-    {
-        $action = new ShowAction();
+	public function testNotFound() : void
+	{
+		$action = new ShowAction();
 
-        $request = (new ServerRequest())
-            ->withAttribute('id', $id = 10);
+		$request = ( new ServerRequest() )
+			->withAttribute( 'id', 100 );
 
-        $response = $action($request);
+		$response = $action( $request );
 
-        self::assertEquals(404, $response->getStatusCode());
-        self::assertEquals('Undefined page', $response->getBody()->getContents());
-    }
+		self::assertEquals( 404, $response->getStatusCode() );
+		self::assertEquals( 'Undefined page', $response->getBody()->getContents() );
+	}
 }
